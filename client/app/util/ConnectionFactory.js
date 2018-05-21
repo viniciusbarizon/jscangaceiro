26470ad99cc88917dc3a0ddd1ad6db675056d433
Vinicius Barizon <vinicius.barizon@oi.net.br>
2018-05-21 16:26:28 -0300

    Module Pattern

--- client/app/util/ConnectionFactory.js
+++ client/app/util/ConnectionFactory.js
@@ -1,39 +1,49 @@
-const stores = ['tradings'];
+const ConnectionFactory = (() => {
+    const stores = ['tradings'];
 
-class ConnectionFactory {
-    constructor() {
-        throw new Error('Não é possível criar instâncias dessa classe');
+    // starts without connection.
+    let connection = null;
+
+    // returns the class.
+    return class ConnectionFactory {
+        constructor() {
+            throw new Error('Não é possível criar instâncias dessa classe');
+        }
+
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+                if(connection)
+                    return resolve(connection);
+    
+                const openRequest = indexedDB.open('jscangaceiro', 3);
+    
+                openRequest.onupgradeneeded = e => {
+                    // pass the connection to the method.
+                    ConnectionFactory._createStores(e.target.result);
+                };
+    
+                openRequest.onsuccess = e => {
+                    // pass the result (connection) to the promise! Only will be ran one time.
+                    connection = e.target.result;
+                    resolve(e.target.result);
+                };
+    
+                openRequest.onerror = e => {
+                    console.log(e.target.error);
+                    // pass the error to the promise reject!
+                    reject(e.target.error.name);
+                };
+            });
+        }
+    
+        static _createStores(connection) {
+            // iterate in the array to build the stores.
+            store.forEach(store => {
+                if(connection.objectStoreNames.contains(store))
+                    connection.deleteObjectStore(store);
+                
+                connection.createObjectStore(store, {autoIncrement: true});
+            });
+        }
     }
-
-    static getConnection() {
-        return new Promise((resolve, reject) => {
-            const openRequest = indexedDB.open('jscangaceiro', 3);
-
-            openRequest.onupgradeneeded = e => {
-                // pass the connection to the method.
-                ConnectionFactory._createStores(e.target.result);
-            };
-
-            openRequest.onsuccess = e => {
-                // pass the result (connection) to the promise!
-                resolve(e.target.result);
-            };
-
-            openRequest.onerror = e => {
-                console.log(e.target.error);
-                // pass the error to the promise reject!
-                reject(e.target.error.name);
-            };
-        });
-    }
-
-    static _createStores(connection) {
-        // iterate in the array to build the stores.
-        store.forEach(store => {
-            if(connection.objectStoreNames.contains(store))
-                connection.deleteObjectStore(store);
-            
-            connection.createObjectStore(store, {autoIncrement: true});
-        });
-    }
-}
+})();
